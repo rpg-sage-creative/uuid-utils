10354e8fb95b0ed271fe6017eb71224f610d084f
Randal <randal.t.meyer@gmail.com>
2026-02-10 16:57:27 -0800

    remove no longer needed files

--- test/build.mjs
+++
@@ -1,5 +0,0 @@
-/*
-The following import will ensure that all the exported files properly us .js when importing
-This ensures that we will be able to use the util library without the --experimental-modules flag
-*/
-import * as Utils from '../build/index.js';
