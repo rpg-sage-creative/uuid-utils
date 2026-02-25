f1a8a952d545a80e5c5d0066e619819d7e503264
Randal <randal.t.meyer@gmail.com>
2026-02-24 16:09:31 -0800

    support detection of versions 1-8

--- src/UuidRegExp.ts
+++ src/UuidRegExp.ts
@@ -9,7 +9,7 @@ export const UuidRegExp = regex("i")`
 		-
 		[0-9a-f]{4}
 		-
-		[1-5]
+		[1-8]
 		[0-9a-f]{3}
 		-
 		[89ab]
@@ -24,4 +24,4 @@ export const UuidRegExp = regex("i")`
 ` as TypedRegExp<{ uuid:UUID }>;
 */
 
-export const UuidRegExp = /(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)/iv as TypedRegExp<{ uuid:UUID }>;
+export const UuidRegExp = /(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)/iv as TypedRegExp<{ uuid:UUID }>;
